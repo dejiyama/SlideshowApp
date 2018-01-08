5468217eb6219730f444a2c6aae125092ca1370d
dejiyama <32954880+dejiyama@users.noreply.github.com>
2018-01-08 23:01:43 +0900

    imgの配列の代入のところまで、処理は完了。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        image1 = UIImage(named:"img1")
+        image2 = UIImage(named:"img2")
+        image3 = UIImage(named:"img3")
+        image = [image1,image2,image3]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    var image1: UIImage!
+    var image2: UIImage!
+    var image3: UIImage!
+    var image: [UIImage]=[]
+    
 }
 
